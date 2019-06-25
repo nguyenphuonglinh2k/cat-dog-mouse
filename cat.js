3aeaf3284822bf7eda25bce14708b050a6bcf47d
linh <nguyenphuonglinh11102000@gmail.com>
2019-06-25 13:30:46 +0700

    add sayHi method

--- cat.js
+++ cat.js
@@ -1,5 +1,11 @@
-function cat(){
-	console.log('this is module.')
+var chalk = require('chalk');
+
+function cat(name){
+	this.name = name;
+}
+
+cat.prototype.sayHi = function() {
+	console.log('hi!' + ' My name is ' + chalk.red(this.name));
 }
 
 module.exports = cat;
---
+++ index.js
@@ -0,0 +1,4 @@
+var cat = require('./cat');
+
+var cat1 = new cat('Tom');
+cat1.sayHi();
